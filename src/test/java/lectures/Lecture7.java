7b54bea009eb872b220e6ac9aeff742dc75e53d3
Warded120 <ihren@softserveinc.com>
2025-03-26 17:40:39 +0200

    completed lecture 7

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -4,10 +4,10 @@ package lectures;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import beans.Car;
+import beans.Person;
 import com.google.common.collect.ImmutableList;
 import java.math.BigDecimal;
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
@@ -16,17 +16,39 @@ public class Lecture7 {
 
   @Test
   public void count() throws Exception {
+    ImmutableList<Person> people = MockData.getPeople();
 
+    long femalesCount = people.stream()
+            .filter(person -> person.getGender().equalsIgnoreCase("female"))
+            .count();
+
+    System.out.println(femalesCount);
   }
 
   @Test
   public void min() throws Exception {
+    ImmutableList<Car> cars = MockData.getCars();
 
+    double yellowMinPrice = cars.stream()
+            .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+            .mapToDouble(Car::getPrice)
+            .min()
+            .getAsDouble();
+
+    System.out.println(yellowMinPrice);
   }
 
   @Test
   public void max() throws Exception {
+    ImmutableList<Car> cars = MockData.getCars();
 
+    double yellowMaxPrice = cars.stream()
+            .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+            .mapToDouble(Car::getPrice)
+            .max()
+            .getAsDouble();
+
+    System.out.println(yellowMaxPrice);
   }
 
 
@@ -34,17 +56,23 @@ public class Lecture7 {
   public void average() throws Exception {
     List<Car> cars = MockData.getCars();
 
+    double avCarPrice = cars.stream()
+            .mapToDouble(Car::getPrice)
+            .average()
+            .orElseThrow(() -> new RuntimeException("No cars found"));
+
+    System.out.println(avCarPrice);
   }
 
   @Test
   public void sum() throws Exception {
     List<Car> cars = MockData.getCars();
+
     double sum = cars.stream()
-        .mapToDouble(Car::getPrice)
-        .sum();
-    BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
+            .mapToDouble(Car::getPrice)
+            .sum();
+
     System.out.println(sum);
-    System.out.println(bigDecimalSum);
 
   }
 
