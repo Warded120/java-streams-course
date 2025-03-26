0a1fd863d658ccf6d0f89a6ae61bbed816f81fbd
Warded120 <ihren@softserveinc.com>
2025-03-26 18:46:11 +0200

    completed lecture 8

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableList;
 import mockdata.MockData;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
@@ -16,7 +18,15 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+    ImmutableList<Car> cars = MockData.getCars();
 
+    Map<String, List<Car>> collectedCars = cars.stream()
+            .collect(Collectors.groupingBy(Car::getMake));
+
+    collectedCars.forEach((make, makeCars) -> {
+      System.out.println("------ " + make + " ------");
+      makeCars.forEach(System.out::println);
+    });
   }
 
   @Test
@@ -34,7 +44,12 @@ public class Lecture8 {
             "Alex"
         );
 
+    Map<String, Long> groupedNames = names.stream()
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
+    groupedNames.forEach((name, count) -> {
+      System.out.println(name + ": " + count);
+    });
   }
 
 }
