c6e3511f2524f657948fd05da65b5d0ebc3fb1d4
Warded120 <ihren@softserveinc.com>
2025-03-26 13:38:32 +0200

    completed lecture 3

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.collect.ImmutableList;
 import java.util.Comparator;
 import java.util.List;
+
 import org.junit.Test;
 
 public class Lecture3 {
@@ -13,11 +14,22 @@ public class Lecture3 {
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    Integer minimum = numbers.stream()
+            .min(Comparator.naturalOrder())
+            .get();
+
+    assertThat(minimum).isEqualTo(1);
+    System.out.println(minimum);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    Integer maximum = numbers.stream()
+            .max(Comparator.naturalOrder())
+            .get();
 
+    assertThat(maximum).isEqualTo(100);
+    System.out.println(maximum);
   }
 }
