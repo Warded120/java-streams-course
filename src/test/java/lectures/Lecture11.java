f2cb8a2c104f8410fdc47b2be6776d0d3cf890cc
Warded120 <ihren@softserveinc.com>
2025-03-27 10:12:09 +0200

    completed lecture 11

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -12,11 +12,19 @@ public class Lecture11 {
   public void joiningStrings() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < names.size()-1; i++) {
+      builder.append(names.get(i));
+      builder.append(",");
+    }
+    builder.append(names.get(names.size()-1));
+    System.out.println(builder.toString());
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    String joined = names.stream().collect(Collectors.joining(","));
+    System.out.println(joined);
   }
 }
